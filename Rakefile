a7084c1623c8e062e9837e9fe7e815565663d1da
Burke Libbey <burke@burkelibbey.org>
2010-01-13 10:19:19 -0600

    inital

---
+++ Rakefile
@@ -0,0 +1,44 @@
+require 'rubygems'
+require 'rake'
+
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gem|
+    gem.name = "multilogger"
+    gem.summary = "Split Rails logging into multiple log files"
+    gem.description = "Multilogger pattern matches incoming log messages to decide which log file to write them to."
+    gem.email = "burke@burkelibbey.org"
+    gem.homepage = "http://github.com/burke/multilogger"
+    gem.authors = ["Burke Libbey"]
+    gem.files.include '{spec,lib}/**/*'
+  end
+rescue LoadError
+  puts "Jeweler (or a dependency) not available. Install it with: sudo gem install jeweler"
+end
+
+begin
+  require 'rcov/rcovtask'
+  Rcov::RcovTask.new do |test|
+    test.libs << 'spec'
+    test.pattern = 'spec/**/*_spec.rb'
+    test.verbose = true
+  end
+rescue LoadError
+  task :rcov do
+    abort "RCov is not available. In order to run rcov, you must: sudo gem install spicycode-rcov"
+  end
+end
+
+require 'rake/rdoctask'
+Rake::RDocTask.new do |rdoc|
+  if File.exist?('VERSION')
+    version = File.read('VERSION')
+  else
+    version = ""
+  end
+
+  rdoc.rdoc_dir = 'rdoc'
+  rdoc.title = "multilogger #{version}"
+  rdoc.rdoc_files.include('README*')
+  rdoc.rdoc_files.include('lib/**/*.rb')
+end
---
+++ lib/multilogger.rb
@@ -0,0 +1,42 @@
+# Extend rails' logger to support multiple targets.
+# Patterns are added with `add_log`, then when a message is added to the log,
+# it is checked against the patterns.
+# example:
+#     logger = MultiLogger.new("log.txt")
+#     logger.add_log("Delayed::Job", "dj_log.txt")
+class MultiLogger < ActiveSupport::BufferedLogger
+
+  # pattern can be either a String or a Regexp.
+  def add_log(pattern, target)
+    @extra_logs[pattern] = ActiveSupport::BufferedLogger.new(target)
+    return self # just so we can't accidentally grab the sub-log and do something stupid with it.
+  end
+
+  def initialize(target, level=DEBUG)
+    super(target, level)
+    @extra_logs = {}
+  end
+
+  alias __old_add add
+
+  def add(severity, message=nil, progname=nil, &block)
+    @extra_logs.keys.each do |pattern|
+      if message.match(pattern)
+        return @extra_logs[pattern].add(severity, message, progname, &block)
+      end
+    end
+    __old_add(severity, message, progname, &block)
+  end
+
+  # Set a few methods to also call on all the defined sub-logs.
+  # This is handled a little inefficiently, but these won't be called often.
+  [:auto_flush, :buffer, :clear_buffer, :silence, :auto_flushing=, :flush, :close].each do |method|
+    alias_method "__old_#{method}".to_sym, method
+    define_method method do |*args|
+      @extra_logs.values.each{|e|e.method(method).call(*args)}
+      method("__old_#{method}").call(*args)
+    end
+  end
+  protected :auto_flush, :buffer, :clear_buffer
+  
+end
